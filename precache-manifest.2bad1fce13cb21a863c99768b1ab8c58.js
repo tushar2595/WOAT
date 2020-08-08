80fe9fe9b963c8ad3ecaa1f7a853d38561a8694e
tushar2595 <44287313+tushar2595@users.noreply.github.com>
2020-08-08 14:50:33 +0530

    Updates

---
+++ precache-manifest.2bad1fce13cb21a863c99768b1ab8c58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e502fd3a527c80292d625495ee1d16d6",
+    "url": "/.github.io/WOAT/index.html"
+  },
+  {
+    "revision": "fb377db03e1713f81e38",
+    "url": "/.github.io/WOAT/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "11345f198cabdffe13e4",
+    "url": "/.github.io/WOAT/static/css/main.1925c372.chunk.css"
+  },
+  {
+    "revision": "fb377db03e1713f81e38",
+    "url": "/.github.io/WOAT/static/js/2.9f156700.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/.github.io/WOAT/static/js/2.9f156700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11345f198cabdffe13e4",
+    "url": "/.github.io/WOAT/static/js/main.aa88bf45.chunk.js"
+  },
+  {
+    "revision": "7acbea8b4a42e4c63453",
+    "url": "/.github.io/WOAT/static/js/runtime-main.18caf936.js"
+  }
+]);
